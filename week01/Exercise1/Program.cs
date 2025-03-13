f8d2c60a0e4df7d96f4f10aee97f7ccc2427ed44
francis-2008-happy <francisbucci2@gmail.com>
2025-03-13 13:50:10 +0100

    modify

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -5,5 +5,8 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        int number = 5;
+        int input = int.Parse(Console.ReadLine());
+        Console.WriteLine($"Press any key {}.");
     }
 }
